002b108d0b9f5523e4f29632883af1f57bfee7cc
Maxime Hu <96578270+Sikoual@users.noreply.github.com>
2022-05-23 19:59:32 +0200

    Update app.js
    J’ai modifié le cs, est-ce mieux? ou sinon je suis vraiment hors sujet et j’ai rien compris lol...

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Exercice 1
 
-const imc = (poids,tailleEnMetre) => {
-	return poids/tailleEnMetre**2;
+const imc = (poids, tailleEnMetre) => {
+	return poids / tailleEnMetre ** 2;
 }
 
-const bernardIMC1   = imc(78,1.69);
-const bernardIMC2   = imc(95,1.88);
-const marcelIMC1    = imc(92,1.95);
-const marcelIMC2    = imc(85,1.76);
+const bernardIMC1 = imc(78, 1.69);
+const bernardIMC2 = imc(95, 1.88);
+const marcelIMC1 = imc(92, 1.95);
+const marcelIMC2 = imc(85, 1.76);
 
 const bernardHigherIMC = bernardIMC1 > marcelIMC1;
 console.log(bernardHigherIMC);
@@ -21,16 +21,16 @@ console.log(`Bernard a un IMC(${bernardIMC1.toFixed(2)}) plus élevé que Marcel
 // Exercice 3
 
 const resultImc = (nom, imc) => {
-		if (imc < 20){
-			console.log(`${nom} : insuffisance pondérale`);
-		} else if (imc <= 25){
-			console.log(`${nom} : poids normal`);
-		} else if (imc <= 30){
-			console.log(`${nom} : surpoids`);
-		} else{
-			console.log(`${nom} : adiposité sévères`);
-		}
+	if (imc < 20) {
+		console.log(`${nom} : insuffisance pondérale`);
+	} else if (imc <= 25) {
+		console.log(`${nom} : poids normal`);
+	} else if (imc <= 30) {
+		console.log(`${nom} : surpoids`);
+	} else {
+		console.log(`${nom} : adiposité sévères`);
+	}
 }
 
 resultImc("Bernard", bernardIMC2);
-resultImc("Marcel",marcelIMC2);
+resultImc("Marcel", marcelIMC2);
