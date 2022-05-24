b0367c00f98af04693e2f6b6bd94eb1d522af23e
MaximeHu <maximeh.devweb@gmail.com>
2022-05-24 13:59:17 +0200

    Challenge6 : compétition de gymnastique, le retour

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const winner = (scoreDauphins, scoreKoalas) => {
-	const averageDauphins = scoreDauphins.reduce((total, acc) => total + acc, 0) / scoreDauphins.length;
-	const averageKoalas = scoreKoalas.reduce((total, acc) => total + acc, 0) / scoreKoalas.length;
-	
-	const maxScore = Math.max(averageDauphins, averageKoalas)
-	
-	if (maxScore <= 100) {
-		return `Aucune équipe n'a de moyenne supérieur à 100, pas de vainqueur`
-	} else if (averageDauphins > averageKoalas) {
-		return `L'équipe gagnante de la compétition est l'équipe Dauphin !`
-	} else if (averageDauphins < averageKoalas) {
-		return `L'équipe gagnante de la compétition est l'équipe Koalas ! `
-	} else {
-		return `Les deux équipes sont à égalités ! `
-	}
+const calcAverage = (scores) => {
+	return scores.reduce((total, acc) => total + acc, 0) / scores.length;
 }
 
-console.log(winner([96, 108, 89], [88, 91, 110]))
-console.log(winner([97, 112, 101], [109, 95, 123]))
-console.log(winner([97, 112, 101], [109, 95, 106]))
+// DONNEE 1
+const averageDauphins1 = calcAverage([44, 23, 71]) // 46
+const averageKoalas1 = calcAverage([65, 54, 49]) // 56
+
+// DONNEE 2
+
+const averageDauphins2 = calcAverage([85, 54, 41]) // 60
+const averageKoalas2 = calcAverage([23, 34, 27]) // 28
+
+const checkWinner = (averageDauphins, averageKoalas) => {
+	return averageDauphins > averageKoalas ? `L'équipe Dauphins gagne (${averageDauphins} vs ${averageKoalas})` : `L'équipe Koalas gagne (${averageKoalas} vs ${averageDauphins})`
+}
+
+console.log(checkWinner(averageDauphins1, averageKoalas1))
+console.log(checkWinner(averageDauphins2, averageKoalas2))
