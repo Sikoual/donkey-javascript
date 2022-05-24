dc7e2d66f30d271a8785d4d694e281a11d9edc5f
Maxime Hu <96578270+Sikoual@users.noreply.github.com>
2022-05-24 14:04:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const tips = (checkAmount) => {
-	return checkAmount > 50 && checkAmount < 300 ? checkAmount * 0.15 : checkAmount * 0.20;
+	return checkAmount > 50 && checkAmount < 300 
+		? checkAmount * 0.15 
+		: checkAmount * 0.20;
 }
 
 console.log(tips(275));
