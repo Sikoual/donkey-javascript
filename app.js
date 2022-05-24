bb6278c458fe3171cbf3d0d4e3fb9ea601ce224d
MaximeHu <maximeh.devweb@gmail.com>
2022-05-24 13:41:26 +0200

    Challenge5: tip calculator

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-Initial commit
+const tips = (checkAmount) => {
+	return checkAmount > 50 && checkAmount < 300 ? checkAmount * 0.15 : checkAmount * 0.20;
+}
+
+console.log(tips(275));
+console.log(tips(40));
+console.log(tips(430));
