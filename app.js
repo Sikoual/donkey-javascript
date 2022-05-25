d737259cf6830b94a3005b983eb2fcb4427e0412
MaximeHu <maximeh.devweb@gmail.com>
2022-05-25 08:17:01 +0200

    Challenge12 : Evelyn Deliapp

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-Initial commit
+// DONNE 1
+const temperatures1 = [17, 21, 23];
+let day = 1;
+
+for(let temperature of temperatures1){
+	if(day === 1){
+		console.log(`${temperature} degrés dans ${day} jour`)
+		day++
+	}else{
+		console.log(`${temperature} degrés dans ${day} jours`)
+		day++
+	}
+}
+
+// DONNE 2
+
+const temperatures2 = [12, 5, -5, 0, 4];
+
+for(let temperature of temperatures2){
+	if(day === 1){
+		console.log(`${temperature} degrés dans ${day} jour`)
+		day++
+	}else{
+		console.log(`${temperature} degrés dans ${day} jours`)
+		day++
+	}
+}
