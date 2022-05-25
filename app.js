70908cbd3b568b0defa1a0011a8308d32d46e245
MaximeHu <maximeh.devweb@gmail.com>
2022-05-25 07:40:04 +0200

    Challenge 10

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-Initial commit
+const aries = `♈`,
+	taurus = `♉`,
+	gemini = `♊`,
+	cancer = `♋`,
+	leo = `♌`,
+	virgo = `♍`,
+	libra = `♎`,
+	scorpio = `♏`,
+	sagittarius = `♐`,
+	capricorn = `♑`,
+	aquarius = `♒`,
+	pisces = `♓︎`;
+
+const fireSigns = [aries, leo];
+const earthSigns = [taurus, virgo, capricorn, sagittarius];
+const airSigns = [gemini, libra, aquarius];
+const waterSigns = [cancer, scorpio, pisces];
+
+fireSigns.push(sagittarius);
+
+console.log(
+	fireSigns[fireSigns.length - 1] === "♐"
+		? "Good Answer ✅"
+		: "Wrong Answer ❌"
+);
+
+earthSigns.pop();
+
+console.log(
+	earthSigns[earthSigns.length - 1] !== "♐"
+		? "Good Answer ✅"
+		: "Wrong Answer ❌"
+);
+
+const array = [];
+let zodiacSigns = array.concat(fireSigns, earthSigns, waterSigns, airSigns);
+
+
+console.log(
+	(zodiacSigns !== null && zodiacSigns.toString() === "♈,♌,♐,♉,♍,♑,♋,♏,♓︎,♊,♎,♒")
+		? "Good Answer ✅"
+		: "Wrong Answer ❌"
+);
+
+console.log(`There is ${zodiacSigns.length} signs in the zodiac`);
